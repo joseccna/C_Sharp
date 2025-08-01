b020219b427b28aa61d630035d818f36f8985069
jose.ccna <jose.ccna@gmail.com>
2025-08-01 20:28:12 -0300

    inicio da segunda aual com conversor de temperatura.

---
+++ cadastro pessoal/cadastro pessoal/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Cadastro pessoal!");
+Console.WriteLine("Digigte seu nome: ");
+string nome = Console.ReadLine() ?? "";
+Console.WriteLine("Digite sua idade: ");
+int idade = Convert.ToInt32(Console.ReadLine() );
+Console.WriteLine("Digite sua altura: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Olá {nome}! Voce tem {idade} anos e mede {altura}.");
+
---
+++ conversor_temperatura/conversor_temperatura/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Conversor de temperatura!");
+Console.WriteLine("Digite a temperatura em Celsius: ");
+double celsius = Convert.ToDouble(Console.ReadLine());
+double fahrenheit = (celsius + 9 / 5) + 32;
+Console.WriteLine($"A temperatura em Fahrenheit é: {fahrenheit:F1}°F");
+
