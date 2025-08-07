5501f88232ab2341281437447a5012b758a62fb4
jose.ccna <jose.ccna@gmail.com>
2025-08-06 21:26:30 -0300

    For Tabuada.

---
+++ for_tabuada/for_tabuada/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Tabuada");
+Console.Write("Digite um número para gerar a tabuada: ");
+int tabuada = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= 10; i++)
+{
+    int resultado = tabuada * i;
+    Console.WriteLine($"{tabuada} x {i} =  {resultado}");
+}
