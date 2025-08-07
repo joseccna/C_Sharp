896ff7d8f0df47b627cedce09f21fb4edd6675f2
jose.ccna <jose.ccna@gmail.com>
2025-08-06 22:14:57 -0300

    Vetor no csharp

---
+++ vetor_array/vetor_array/Program.cs
@@ -0,0 +1,50 @@
+﻿// Vetor
+
+//int[] numero = new int[5];
+
+//numero[0] = 10;
+//numero[1] = 20;
+//numero[2] = 30;
+//numero[3] = 40;
+//numero[4] = 50;
+
+//for (int i = 0; i < numero.Length; i++)
+//{
+//    Console.WriteLine($"Posição {i} = {numero[i]}");
+//}
+
+/////////////////// outra forma de declarar vetor
+
+//string[] nome = new string[3] { "maria", "João", "Ana" };
+//for (int i =0; i < nome.Length; i++)
+//{
+//    Console.WriteLine($"Posição {i} = {nome[i]}");
+//}
+
+////////////////// outra forma de percorrer vetor
+
+//string[] nome = new string[3] { "maria", "João", "Ana" };
+
+//foreach (string n in nome)
+//{
+//    Console.WriteLine(n);
+//}
+
+/////////////////// nota
+
+double[] nota = new double[3];
+
+for (int i = 0; i < nota.Length; i++)
+{
+    Console.Write($"Digite a nota {i + 1}: ");
+    nota[i] = Convert.ToDouble(Console.ReadLine());
+
+    Console.Clear();
+}
+
+Console.WriteLine("Notas digitadas: ");
+
+for (int i = 0; i < nota.Length; i++)
+{
+    Console.WriteLine($"Nota {i + 1} = {nota[i]}");
+}
