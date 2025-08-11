9cd9ff50880cd77d5a6bb15b029206969fed97bf
jose.ccna <jose.ccna@gmail.com>
2025-08-11 19:56:26 -0300

    Cadastro de produto com array

---
+++ Cadastro de Produtos/Cadastro de Produtos/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Cadastro de Produtos");
+string[] produtos = new string[5];
+double[] precos = new double[5];
+
+for (int i = 0; i < produtos.Length; i++)
+{
+    Console.Write("Digite o nome do produto: ");
+    produtos[i] = Console.ReadLine();
+    
+    Console.Write("Digite o preço do produto: ");
+    precos[i] = Convert.ToDouble(Console.ReadLine());
+    Console.Clear();
+}
+for (int i = 0; i < produtos.Length; i++)
+{
+    Console.WriteLine($"Produto: {produtos[i]}, Preço:R$ {precos[i]:C2}");
+}
