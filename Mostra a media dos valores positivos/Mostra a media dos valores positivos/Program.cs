2d11b9c79a7e747400119d21a72054dbce6ff567
jose.ccna <jose.ccna@gmail.com>
2025-08-11 20:23:09 -0300

    Media de valores positivos com while

---
+++ Mostra a media dos valores positivos/Mostra a media dos valores positivos/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Mostra as meida dos valores positivos");
+Console.WriteLine("Digite os valores (digite 0 para terminar):");
+int contador = 0;
+double soma = 0.0;
+while (true)
+{
+        double valor = Convert.ToDouble(Console.ReadLine());
+    if (valor == 0) {
+        break;
+    }
+    if (valor > 0) {
+        soma += valor;
+        contador++;
+    }
+}
+double media =  soma / contador;
+Console.WriteLine($"A média dos valores positivos é: {media}");
