fba965f0abb394d6cac7dc02a0eb0e969ddc7ed0
jose.ccna <jose.ccna@gmail.com>
2025-08-11 21:47:33 -0300

    apresentar dia na semana com switch case

---
+++ semana/semana/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Digite um numero do 1 ao 7");
+string dia = Console.ReadLine() ??"";
+
+switch(dia){
+    case "1":
+        Console.WriteLine("Domingo");
+        break;
+    case "2":
+        Console.WriteLine("Segunda-feira");
+        break;
+    case "3":
+        Console.WriteLine("Terça-feira");
+        break;
+    case "4":
+        Console.WriteLine("Quarta-feira");
+        break;
+    case "5":
+        Console.WriteLine("Quinta-feira");
+        break;
+    case "6":
+        Console.WriteLine("Sexta-feira");
+        break;
+    case "7":
+        Console.WriteLine("Sábado");
+        break;
+    default:
+        Console.WriteLine("Número inválido.");
+        break;
+}
+
