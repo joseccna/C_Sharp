29b1480fc683091d32546fd4e7690bb2f004d069
jose.ccna <jose.ccna@gmail.com>
2025-08-13 20:17:55 -0300

    inicio programa orientado por objeto.

---
+++ objeto inicio/objeto inicio/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Carro
+{
+    public string marca = "Toyota";
+    public string modelo = "Corolla";
+    public int ano = 2008;
+
+    public void MostrarDetalhes()
+    {
+        Console.WriteLine($"Marca: {marca}, Modelo: {modelo}, Ano: {ano}");
+    }
+
+}
