3436de98bf30f51561899c99ed02c248fb47205a
jose.ccna <jose.ccna@gmail.com>
2025-08-13 22:10:20 -0300

    POO Livraria.

---
+++ Livraria/Livraria/Livro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria
+{
+    public class Livro
+    {
+        public string Titulo;
+        public string Autor;
+        public int Ano;
+
+        public void ExibirDetalhes()
+        {
+            Console.WriteLine($"Título: {Titulo}, Autor: {Autor}, Ano: {Ano}");
+        }
+    
+
+    public void Atualizacao(int novoAno)
+        {
+            Ano = novoAno;
+
+            //if (ano < 0)
+            //{
+            //    throw new ArgumentException("O ano não pode ser negativo.");
+            //}
+            //Ano = ano;
+        }
+
+        public int CalcularIdade()
+        {
+
+            //ano atual - ano do livro
+            return DateTime.Now.Year - Ano;// Retorna a idade do livro
+            
+        }
+    }
+    
+}
+
---
+++ Livraria/Livraria/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Livraria;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Livro l1 = new Livro();  // Usando inicializador de objeto
+
+        l1.Titulo = "O Senhor dos Anéis";
+        l1.Autor = "J.R.R. Tolkien";
+        l1.Ano = 1954;
+
+        //Criando um segundo livro usando inicializador de objeto 
+        //de forma mais concisa
+
+        Livro l2 = new Livro
+        {
+            Titulo = "1984",
+            Autor = "George Orwell",
+            Ano = 1949
+        };
+
+        //Console.WriteLine($"Título: {l1.Titulo} - Título: {l2.Titulo}");
+        //Console.WriteLine($"Autor: {l1.Autor} - Autor: {l2.Autor}");
+        //Console.WriteLine($"Ano: {l1.Ano} - Ano: {l2.Ano}");
+
+        l1.Atualizacao(2023); // Atualizando o ano do primeiro livro
+
+        l1.ExibirDetalhes();
+        l2.ExibirDetalhes();
+
+        Console.WriteLine($"Idade do livro '{l2.Titulo}': {l2.CalcularIdade()} anos");
+    }
+}
