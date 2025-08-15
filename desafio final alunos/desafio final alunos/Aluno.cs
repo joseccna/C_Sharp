7e43603ce1a638cf034f34543b04c62db1593781
jose.ccna <jose.ccna@gmail.com>
2025-08-15 20:09:05 -0300

    POO media nota array

---
+++ desafio final alunos/desafio final alunos/Aluno.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio_final_alunos
+{
+    public class Aluno
+    {
+
+        public string nome = "";
+        public double nota1;
+        public double nota2;
+
+        public void LerDados()
+        {
+            Console.Write("Digite o nome do aluno: ");
+            nome = Console.ReadLine() ??"";
+            Console.Write("Digite a nota 1: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite a nota 2: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double CalcularMedia()
+        {
+            return (nota1 + nota2) / 2;
+        }   
+        public void ExibirResultado()
+        {
+            double media = CalcularMedia();
+            Console.WriteLine($"Aluno: {nome}");
+            Console.WriteLine($"Nota 1: {nota1}");
+            Console.WriteLine($"Nota 2: {nota2}");
+            Console.WriteLine($"Média: {media}");
+            if (media >= 7)
+            {
+                Console.WriteLine("Situação: Aprovado");
+            }
+            else if (media >= 5)
+            {
+                Console.WriteLine("Situação: Recuperação");
+            }
+            else
+            {
+                Console.WriteLine("Situação: Reprovado");
+            }
+        }   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ desafio final alunos/desafio final alunos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using desafio_final_alunos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno[] alunos = new Aluno[3];
+
+        for (int i = 0; (i) < alunos.Length; (i)++)
+        {
+            alunos[i] = new Aluno();
+            alunos[i].LerDados();
+            
+        }
+        Console.Clear();
+        for (int i = 0; i < alunos.Length; i++)
+        {
+            alunos[i].ExibirResultado();
+            Console.WriteLine(); // Linha em branco para separar os resultados
+        }
+
+        
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+}
