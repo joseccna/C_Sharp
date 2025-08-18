eef87cefd9108d4bec38a38049cdbac084bdc171
jose.ccna <jose.ccna@gmail.com>
2025-08-18 20:22:19 -0300

    Utilizando o get e set automaizado.

---
+++ Treino_POO_classe/Treino_POO_classe/Album.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treino_POO_classe
+{
+    internal class Album
+    {
+        public string Artista { get; set; } = string.Empty;
+        public int AnoLancamento { get; set; }
+        public string Gereno { get; set; } = string.Empty;  
+
+
+        public void ExibirDetalhes()
+        {
+            Console.WriteLine($"Artista: {Artista}");
+            Console.WriteLine($"Ano de Lançamento: {AnoLancamento}");
+            Console.WriteLine($"Gênero: {Gereno}");
+        }
+        public void ExibirDetalhes(string prefixo)
+        {
+            Console.WriteLine($"{prefixo} Artista: {Artista}");
+            Console.WriteLine($"{prefixo} Ano de Lançamento: {AnoLancamento}");
+            Console.WriteLine($"{prefixo} Gênero: {Gereno}");
+        }
+
+
+    }
+}
---
+++ Treino_POO_classe/Treino_POO_classe/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Treino_POO_classe;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Album album = new Album
+        {
+            Artista = "The Beatles",
+            AnoLancamento = 1969,
+            Gereno = "Rock"
+        };
+        // Exibindo detalhes do álbum
+        album.ExibirDetalhes();
+        // Exibindo detalhes com prefixo
+        album.ExibirDetalhes("Prefixo: ");
+        
+
+    }
+}
