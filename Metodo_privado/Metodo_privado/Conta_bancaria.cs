411de5e49cab3fa3358b28547c5294b64a516272
jose.ccna <jose.ccna@gmail.com>
2025-08-18 22:19:21 -0300

    fim da aula

---
+++ Metodo_privado/Metodo_privado/Conta_bancaria.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodo_privado
+{
+    internal class Conta_bancaria
+    {
+        private string titular = "";
+        private double saldo = 0.0;
+
+        public void Titular(string nome)
+        {
+            titular = nome;
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                saldo += valor;
+            }
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor > 0 && valor <= saldo)
+            {
+                saldo -= valor;
+            }
+        }
+
+        public double ObterSaldo()
+        {
+            return saldo;
+        }
+
+        public string Titular1
+        {
+            get { return titular; }
+
+
+        }
+
+    }
+}
---
+++ Metodo_privado/Metodo_privado/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Metodo_privado;
+
+class Program
+{
+     public static void Main(string[] args)
+     {
+        Conta_bancaria conta = new Conta_bancaria();
+        conta.Titular("João da Silva");
+        conta.Depositar(1000.0);
+        conta.Sacar(200.0);
+
+        Console.WriteLine($"Titular: {conta.Titular1}"); // Exibe o titular da conta
+        Console.WriteLine($"Saldo atual: {conta.ObterSaldo()}"); // Exibe o saldo atual
+    }
+}
